7a05a9a4a0a1bbad70c8ef07099753f32bdca798
Zulfiqar Tamin <fiqartamin@gmail.com>
2024-05-29 10:14:59 +0700

    git ignore

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'success' => true,
+        'message' => "Welcome",
+    ], 200);
 });
