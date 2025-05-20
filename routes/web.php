9d913b4c6ab6aec09a27ce5a1ef5ef65d4c8d7ae
Zulfiqar Tamin <fiqartamin@gmail.com>
2025-05-20 21:18:27 +0700

    landing page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return response()->json([
+//         'success' => true,
+//         'message' => "Welcome RONI API",
+//     ], 200);
+// });
+
 Route::get('/', function () {
-    return response()->json([
-        'success' => true,
-        'message' => "Welcome RONI API",
-    ], 200);
+    return view('ronilanding');
 });
